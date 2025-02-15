1f188493bda612f5608cba2c48c24c3434a0ed03
daadestroyer <nigamshubham2000@gmail.com>
2025-02-15 16:36:59 +0530

    Created user service

---
+++ microservices-lcwd/src/main/java/com/thecoderstv/userservice/MicroservicesLcwdApplication.java
@@ -0,0 +1,13 @@
+package com.thecoderstv.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesLcwdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesLcwdApplication.class, args);
+	}
+
+}
