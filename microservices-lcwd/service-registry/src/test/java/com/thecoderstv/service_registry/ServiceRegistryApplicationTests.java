3218644c55178d119814f2896a82e40ad9885e57
daadestroyer <nigamshubham2000@gmail.com>
2025-02-18 23:06:36 +0530

    Implemented service registry microservice...

---
+++ microservices-lcwd/service-registry/src/test/java/com/thecoderstv/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecoderstv.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
